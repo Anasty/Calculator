e89b681f88b8218ef4de8ff4d2dcc95f411f3bdb
Anasty <anastymail@gmail.com>
2020-01-17 13:47:35 +0700

    Initial commit

---
+++ Calculator/CalculatorClass.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class CalculatorClass
+    {
+        static public double Calculate(string input)
+        {
+            string rpn = RPN(input);          
+
+            return Compute(rpn);
+        }
+
+        static bool IsOperator(char c)
+        {
+            if (("+-/*^() ".IndexOf(c) != -1))
+                return true;
+            return false;
+        }
+
+        static byte Priority(char c)
+        {
+            switch (c)
+            {
+                case '+': return 1;
+                case '-': return 1;
+                case '*': return 2;
+                case '/': return 2;              
+                default: return 0;
+            }
+        }
+
+        static string RPN(string input)
+        {
+            string output = string.Empty;
+            Stack<char> operators = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsDigit(input[i]) || input[i]== ',')
+                {
+                    if (input[i] == ',')
+                        output += "0";
+                    while (!IsOperator(input[i]))
+                    { 
+                        output += input[i];                        
+                        if (i+1 == input.Length || IsOperator(input[i+1]))
+                            break;
+                        i++;
+                    }
+                    output += " ";
+                }
+               
+
+                if (IsOperator(input[i]))
+                {
+                    if (input[i] == '(')
+                        operators.Push(input[i]);
+                    else if (input[i] == ')')
+                    {
+                        try
+                        {
+                            char oper = operators.Pop();
+                            while (oper != '(')
+                            {
+                                output += oper.ToString();
+                                oper = operators.Pop();
+                            }
+                        }
+                        catch(InvalidOperationException ex)
+                        {
+                            System.Windows.MessageBox.Show("Ошибка! Некорректный ввод", ex.Message);
+                            return "0";
+                        }
+                    }
+                    else
+                    {
+                        if(operators.Count > 0)
+                            if (Priority(input[i]) <= Priority(operators.Peek()))
+                            {
+                                output += operators.Pop().ToString();
+                            }
+                        operators.Push(char.Parse(input[i].ToString()));
+                    }
+                }
+                
+            }
+            while (operators.Count > 0)
+            {
+                output += operators.Pop();                
+            }
+            return output;
+        }
+
+        static double Compute(string parseInput)
+        {
+            double result = 0;
+
+            Stack<double> tempStack = new Stack<double>();
+            if (parseInput.Length > 0)
+            { 
+
+                for (int i = 0; i < parseInput.Length; i++)
+                {
+                    if (parseInput[i] == ' ')
+                        continue;
+
+                    if (Char.IsDigit(parseInput[i]))
+                    {
+                        string number = string.Empty;
+                        while (!IsOperator(parseInput[i]))
+                        {
+                            number += parseInput[i];
+                            if (i + 1 == parseInput.Length || IsOperator(parseInput[i + 1]))
+                                break;
+                            i++;
+                        }
+                        tempStack.Push(double.Parse(number));
+                    }
+                    else if (IsOperator(parseInput[i]))
+                    {
+                        try
+                        {
+                            double secondNumber;
+                            double firstNumber = tempStack.Pop();
+
+                            if (tempStack.Count == 0) //обработка чисел с минусом
+                            {
+                                secondNumber = 0;
+                            }
+                            else
+                                secondNumber = tempStack.Pop();
+
+                            switch (parseInput[i])
+                            {
+                                case '+': result = firstNumber + secondNumber; break;
+                                case '-': result = secondNumber - firstNumber; break;
+                                case '*': result = firstNumber * secondNumber; break;
+                                case '/':
+                                    if (firstNumber != 0)
+                                    {
+                                        result = secondNumber / firstNumber;
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        System.Windows.MessageBox.Show("Ошибка! Деление на 0");
+                                        return 0;
+                                    }
+                                default:
+                                    System.Windows.MessageBox.Show("Ошибка! Неизвестный оператор");
+                                    break;
+
+                            }
+                            tempStack.Push(result);
+                        }
+                        catch (InvalidOperationException ex)
+                        {
+                            System.Windows.MessageBox.Show("Ошибка! Некорректный ввод", ex.Message);
+                            return 0;
+                        }
+                    }
+                }
+                return tempStack.Peek();
+            }
+            return 0;
+        }
+    }
+    
+}
---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool equalPressed = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string CheckOutput(string inputElement)
+        {
+            if (equalPressed && ("+-/*".IndexOf(inputElement) == -1))//возможность продолжить вычисления после получения результата
+                Output.Text = "";
+
+            if(Output.Text.Length > 0) //нельзя ввести операторы подряд
+            {
+                char lastElement = Output.Text[Output.Text.Length - 1];
+                if (("+-/*".IndexOf(lastElement) != -1) && (("+-/*".IndexOf(inputElement) != -1)))                
+                    Output.Text = Output.Text.Substring(0, Output.Text.Length - 1);                               
+            }
+
+            if (Output.Text.Length > 1) //после открытой скобки можно ввести только число
+            {
+                char lastElement = Output.Text[Output.Text.Length - 1];
+                if (("(".IndexOf(lastElement) != -1) && (("+/*()".IndexOf(inputElement) != -1)))
+                {
+                    Output.Text = Output.Text.Substring(0, Output.Text.Length - 1);
+                    return "(";
+                }
+            }
+            return inputElement;
+        }
+
+        private void ButtonWithValue_Click(object sender, RoutedEventArgs e)
+        {
+            string inputElement = (sender as Button).Content.ToString();
+
+            inputElement = CheckOutput(inputElement);
+
+            Output.Text += inputElement; //добавление нажатой кнопки в строку вывода                      
+            equalPressed = false;
+        }
+
+        private void Button_removeOne_Click(object sender, RoutedEventArgs e)
+        {
+            if(Output.Text.Length > 0)
+                Output.Text = Output.Text.Substring(0, Output.Text.Length - 1); 
+        }
+
+        private void Button_clear_Click(object sender, RoutedEventArgs e)
+        {
+            Output.Text = "";
+            OperationsHistory.Text = "";
+        }        
+
+        private void Button_equal_Click(object sender, RoutedEventArgs e)
+        {
+            OperationsHistory.Text = Output.Text + "=";
+            Output.Text = CalculatorClass.Calculate(Output.Text).ToString();
+
+            equalPressed = true;
+        }
+      
+        private void Window_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {           
+            string inputElement;
+            if (Int32.TryParse(e.Text, out int val) || ("-+/*()".IndexOf(e.Text) != -1))
+            { 
+                inputElement = CheckOutput(e.Text);
+                Output.Text += inputElement;
+                equalPressed = false;
+            }            
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                OperationsHistory.Text = Output.Text + "=";
+                Output.Text = CalculatorClass.Calculate(Output.Text).ToString();
+
+                equalPressed = true;
+            }
+
+            if(e.Key == Key.Back)
+            {
+                if (Output.Text.Length > 0)
+                    Output.Text = Output.Text.Substring(0, Output.Text.Length - 1);
+            }
+        }
+    }
+}
